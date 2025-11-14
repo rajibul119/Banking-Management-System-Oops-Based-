ae840ca986dea30ba43ae97dbf2bda381d84f1ac
rajibul_mollah <mollahrajibul56@gmial.com>
2025-11-14 21:36:35 +0530

    Replaced all imported version and implemented a car class

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# imports 
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-import random 
+class Car:
+    def __init__(self,brand,model,price):
+        self.brand = brand 
+        self.model = model 
+        self.price = price 
+    
+    def print_info(self):
+        return f"CAR INFO\nBrand = {self.brand}\nModel = {self.model}\nPrice = {self.price}"
+    
+c1 = Car("BMW","X30",3000000)
+print(c1.print_info())
+        
